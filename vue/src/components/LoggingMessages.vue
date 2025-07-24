dbbb1f80e2f927a18bdbc37be8f5fed92e13f68b
Shahaz <ali.ghaderipour@gmail.com>
2025-07-24 12:43:30 +0330

    test

--- vue/src/components/LoggingMessages.vue
+++ vue/src/components/LoggingMessages.vue
@@ -3,6 +3,7 @@
     <button class="consoleLog" @click="consoleLog">Log Console</button>
     <button class="consoleWarn" @click="consoleWarn">Log Warning</button>
     <button class="consoleError" @click="consoleError">Log Error</button>
+    <button class="consoleTable" @click="consoleTable">Log Table</button>
   </div>
 </template>
 
@@ -16,10 +17,25 @@ function consoleWarn() {
 function consoleError() {
   console.error('Hello, Error!')
 }
+function consoleTable() {
+  console.table([{
+    name : 'ali',
+    family : 'ghaderi',
+    age : 34
+  },
+    {
+      name : 'ahad',
+      family : 'karimi'
+    },
+    {
+      name : 'majid',
+      statis : 'active'
+    }])
+}
 </script>
 
 <style scoped>
-.consoleLog, .consoleWarn, .consoleError {
+.consoleLog, .consoleWarn, .consoleError, .consoleTable {
   background-color: blue;
   color: white;
   border: 1px solid black;
